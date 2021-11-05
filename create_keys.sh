a25e12792ca7e801d0d9d3ea56be2b070bee8288
Adam Leskis <leskis@gmail.com>
2021-11-05 20:29:47 +0000

    add in terraform config and test for dir before creating keys

--- create_keys.sh
+++ create_keys.sh
@@ -1,1 +1,7 @@
-ssh-keygen -t ed25519 -o -a 100 -f ansible/terraform.ed25519 -N ""
+if [ -d "./ansible" ]
+  then
+    ssh-keygen -t ed25519 -o -a 100 -f ansible/terraform.ed25519 -N ""
+  else
+    mkdir ansible
+    ssh-keygen -t ed25519 -o -a 100 -f ansible/terraform.ed25519 -N ""
+fi
