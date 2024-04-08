da4bfd2c157f7e191052c8f95f414552583bf42a
Adam Leskis <leskis@gmail.com>
2024-04-08 22:55:41 +0100

    update the check script

--- infra/get_a_domain.sh
+++ infra/get_a_domain.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 if [ -z $1 ]; then
-  echo "usage: bash get_a_domain.sh NAME_OF_DOMAIN_HERE.com"
+  echo "usage: ./get_a_domain.sh NAME_OF_DOMAIN_HERE.com"
   exit 1
 fi
 
-DOMAIN=$(python -c "import uuid; print(uuid.uuid4())")-$1
+DOMAIN=$(python3 -c "import uuid; print(uuid.uuid4())")-$1
 AVAILABILITY=$(aws route53domains check-domain-availability --region us-east-1 --domain-name $DOMAIN | jq '.Availability')
 
 if [ $AVAILABILITY != "\"UNAVAILABLE\"" ];
